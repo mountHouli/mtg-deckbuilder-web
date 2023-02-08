59a6d3a24c8c070c731531ba21003eda724a8b19
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2023-02-08 14:57:49 -0700

    [001] Move import of styles-root.scss into index.js (the proper place)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import '../styles/styles-root.scss'
 import Page from './Page'
 import Header from './Header'
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOMClient from 'react-dom/client'
 import App from './components/App';
 
+import './styles/styles-root.scss'
+
 const root = ReactDOMClient.createRoot(document.getElementById('react_root'))
 
 root.render(<App />)
