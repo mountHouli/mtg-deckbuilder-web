b5208413367ee5cfa66b4a4fa982ded22e419559
Aaron Houlihan <AaronRobertHoulihan@gmail.com>
2023-02-08 14:41:25 -0700

    [001] Add card data textarea

--- src/components/CardSearch.jsx
+++ src/components/CardSearch.jsx
@@ -22,7 +22,12 @@ class CardSearch extends React.Component {
     const response = await fetch(url)
     const data = await response.json()
     console.log('TODO: Do something with the data')
-    console.log(data) 
+    console.log(data)
+
+    this.setState({
+      cardName,
+      cardData: JSON.stringify(data, null, 2)
+    })
   }
 
   handleCardNameChange(e) {
@@ -35,11 +40,12 @@ class CardSearch extends React.Component {
     const { cardName, cardData } = this.state
 
     return (
-      <div>
+      <div className="card-search-container">
         <form className="card-search-form">
           <input onChange={(e) => this.handleCardNameChange(e)} value={cardName} className='card-search-input' placeholder="Card name" />
           <button type="submit" onClick={(e) => this.handleSubmit(e)} className='card-search-submit-button'>Look it up!</button>
         </form>
+        <textarea className="card-data-textarea" value={cardData}></textarea>
       </div>
     )
   }
